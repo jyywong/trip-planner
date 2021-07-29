1d403c720b509dd61031de87ec971abc1b0797ba
jyywong <wong.jonathan1@gmail.com>
2021-07-28 21:16:05 -0400

    Finished implementing formik

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import * as Yup from 'yup';
 import { useFormik } from 'formik';
 import { useSnackbar } from 'notistack';
 import { useHistory } from 'react-router';
@@ -6,7 +8,7 @@ import { useDispatch } from 'react-redux';
 import { setCredentials } from '../Slices/AuthSlice';
 import { useLoginMutation } from '../Services/tripPlannerBackend';
 import { useSignupMutation } from '../Services/tripPlannerBackend';
-import { Card, Button, Typography, CardContent, Box, TextField } from '@material-ui/core';
+import { Card, Button, Typography, CardContent, Box, TextField, Link } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import LockIcon from '@material-ui/icons/Lock';
 
@@ -24,7 +26,25 @@ const useStyles = makeStyles({
 		marginBottom: '1rem'
 	}
 });
+
+const SignUpSchema = Yup.object().shape({
+	username: Yup.string()
+		.min(2, 'Username must be more than 2 characters!')
+		.max(30, 'Username must not be longer than 30 characters!')
+		.nullable()
+		.required('A valid username is required!'),
+	email: Yup.string().email('Invalid email').required('A valid email is required!'),
+	password: Yup.string().min(6, 'Password must be more than 6 characters!').required('A valid password is required!'),
+	password2: Yup.string().oneOf([ Yup.ref('password'), null ], 'Passwords must match!')
+});
+
 const SignUp = () => {
+	const { enqueueSnackbar } = useSnackbar();
+	const [ login, { isLoading } ] = useLoginMutation();
+	const [ signup, { isSuccess, isError } ] = useSignupMutation();
+	const dispatch = useDispatch();
+	const classes = useStyles();
+	const { push } = useHistory();
 	const formik = useFormik({
 		initialValues: {
 			username: '',
@@ -32,24 +52,14 @@ const SignUp = () => {
 			password: '',
 			password2: ''
 		},
+		validationSchema: SignUpSchema,
 		onSubmit: (values) => {
-			console.log(values);
+			signup(values).then((response) => console.log(response));
 		}
 	});
-
-	const { enqueueSnackbar } = useSnackbar();
-	const [ login, { isLoading } ] = useLoginMutation();
-	const [ signup, { isSuccess, isError } ] = useSignupMutation();
-	const dispatch = useDispatch();
-	const classes = useStyles();
-	const { push } = useHistory();
-	const [ formValues, setFormValues ] = useState({ username: '', email: '', password: '', password2: '' });
-	const handleSubmit = () => {
-		signup(formValues).then((response) => console.log(response));
-	};
 	const attemptLogin = async () => {
 		try {
-			const user = await login(formValues).unwrap();
+			const user = await login(formik.values).unwrap();
 			dispatch(setCredentials(user));
 			push('/home');
 		} catch (error) {
@@ -72,72 +82,95 @@ const SignUp = () => {
 			<Box display="flex" width="100vw" height="100vh" alignItems="center" justifyContent="center">
 				<Card className={classes.cardOverride} raised>
 					<CardContent className={classes.cardContentOverride}>
-						<Box
-							boxSizing="border-box"
-							height="100%"
-							width="100%"
-							display="flex"
-							flexDirection="column"
-							alignItems="center"
-							padding={1}
-						>
-							<Box display="flex" marginBottom={2} flexDirection="column" alignItems="center">
-								<LockIcon />
-								<Typography variant="h4"> Sign Up </Typography>
-							</Box>
-							<TextField
-								variant="outlined"
-								className={classes.mediumBottomMargin}
-								id="username"
-								label="Username"
-								fullWidth
-								value={formik.username}
-								onChange={formik.handleChange}
-							/>
-							<TextField
-								variant="outlined"
-								className={classes.mediumBottomMargin}
-								id="email"
-								label="Email"
-								type="email"
-								fullWidth
-								value={formik.email}
-								onChange={formik.handleChange}
-							/>
-							<TextField
-								variant="outlined"
-								className={classes.mediumBottomMargin}
-								id="password"
-								label="Password"
-								type="password"
-								fullWidth
-								value={formik.password}
-								onChange={formik.handleChange}
-							/>
-							<TextField
-								variant="outlined"
-								className={classes.mediumBottomMargin}
-								id="password2"
-								label="Confirm Password"
-								type="password"
-								fullWidth
-								value={formik.password2}
-								onChange={formik.handleChange}
-							/>
-							<Button
-								variant="contained"
-								className={classes.smallBottomMargin}
-								color="primary"
-								size="large"
-								fullWidth
-								onClick={handleSubmit}
+						<form onSubmit={formik.handleSubmit} noValidate>
+							<Box
+								boxSizing="border-box"
+								height="100%"
+								width="100%"
+								display="flex"
+								flexDirection="column"
+								alignItems="center"
+								padding={1}
 							>
-								SIGN IN
-							</Button>
-							<Typography className={classes.noBottomMargin} variant="subtitle1">
-								Don't have an account? Sign up!
-							</Typography>
-						</Box>
+								<Box display="flex" marginBottom={2} flexDirection="column" alignItems="center">
+									<LockIcon />
+									<Typography variant="h4"> Sign Up </Typography>
+								</Box>
+								<TextField
+									variant="outlined"
+									className={classes.mediumBottomMargin}
+									id="username"
+									name="username"
+									label="Username"
+									fullWidth
+									{...formik.getFieldProps('username')}
+									error={formik.touched.username && formik.errors.username && true}
+									helperText={
+										formik.touched.username && formik.errors.username ? formik.errors.username : ''
+									}
+								/>
+								<TextField
+									variant="outlined"
+									className={classes.mediumBottomMargin}
+									id="email"
+									name="email"
+									label="Email"
+									type="email"
+									fullWidth
+									{...formik.getFieldProps('email')}
+									error={formik.touched.email && formik.errors.email && true}
+									helperText={formik.touched.email && formik.errors.email ? formik.errors.email : ''}
+								/>
+								<TextField
+									variant="outlined"
+									className={classes.mediumBottomMargin}
+									id="password"
+									name="password"
+									label="Password"
+									type="password"
+									fullWidth
+									{...formik.getFieldProps('password')}
+									error={formik.touched.password && formik.errors.password && true}
+									helperText={
+										formik.touched.password && formik.errors.password ? formik.errors.password : ''
+									}
+								/>
+								<TextField
+									variant="outlined"
+									className={classes.mediumBottomMargin}
+									id="password2"
+									name="password2"
+									label="Confirm Password"
+									type="password"
+									fullWidth
+									{...formik.getFieldProps('password2')}
+									error={formik.touched.password2 && formik.errors.password2 && true}
+									helperText={
+										formik.touched.password2 && formik.errors.password2 ? (
+											formik.errors.password2
+										) : (
+											''
+										)
+									}
+								/>
+								<Button
+									variant="contained"
+									className={classes.smallBottomMargin}
+									color="primary"
+									size="large"
+									fullWidth
+									type="submit"
+								>
+									SIGN UP
+								</Button>
+								<Typography className={classes.noBottomMargin} variant="subtitle1">
+									Already have an account?
+									<Link component={RouterLink} to="/login">
+										Log in!
+									</Link>
+								</Typography>
+							</Box>
+						</form>
 					</CardContent>
 				</Card>
 			</Box>
